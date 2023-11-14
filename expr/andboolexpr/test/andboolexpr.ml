546d118fe45f71c520104945e5d40e1d8a6d3374
Massimo Bartoletti <bart@unica.it>
2023-11-14 17:49:28 +0100

    andboolexpr / tests

--- expr/andboolexpr/test/andboolexpr.ml
+++ expr/andboolexpr/test/andboolexpr.ml
@@ -1,65 +1,48 @@
 open AndboolexprLib.Ast
 open AndboolexprLib.Main
 
-let test_eval expr exp_result =
-  (expr |> parse |> eval) = exp_result
-  
-let%test "test1" = test_eval "false" false
-
-let%test "test2" = test_eval "true" true
-
-let%test "test3" = test_eval "if true then false else true" false  
-
-let%test "test4" = test_eval "if false then false else true" true
-
-let%test "test5" = test_eval "if true then (if true then false else true) else (if true then true else false)" false
-
-let%test "test6" = test_eval "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
-
-let%test "test7" = test_eval "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
-
-let%test "test8" = test_eval "not not true" true
-
-let%test "test9" = test_eval "not if true then false else true" true
-
-let%test "test10" = test_eval "if true then (if true then false else true) else (if true then true else false)" false
-
-let%test "test11" = test_eval "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
-
-let%test "test12" = test_eval "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
-
-let%test "test13" = test_eval "not true or true" true
-
-let%test "test14" = test_eval "not true and false" false
-
-let%test "test15" = test_eval "false and false or true" true
-
-let%test "test16" = test_eval "true or false and false" true
-
-let%test "test17" = test_eval "if true then true else false and false" true
-
-let%test "test18" = test_eval "if true then false else false or true" false
-
-
 (**********************************************************************
  Test big-step semantics
  **********************************************************************)
 
-let%test _ =
-  print_newline();  
-  print_endline ("*** Testing big-step semantics...");
-  List.fold_left
-    (fun b (s,v) ->
-       print_string (s ^ " => ");       
-       let ar = s |> parse |> eval in
-       print_string (string_of_bool ar);
-       let b' = (ar = v) in
-       if b' then print_string(" [OK]")
-       else print_string (" [NO: expected " ^ string_of_bool v ^ "]");
-       print_newline();
-       b && b')
-    true
-    tests
+let test_bigstep expr exp_result =
+  (expr |> parse |> eval) = exp_result
+  
+let%test "test_bigstep1" = test_bigstep "false" false
+
+let%test "test_bigstep2" = test_bigstep "true" true
+
+let%test "test_bigstep3" = test_bigstep "if true then false else true" false  
+
+let%test "test_bigstep4" = test_bigstep "if false then false else true" true
+
+let%test "test_bigstep5" = test_bigstep "if true then (if true then false else true) else (if true then true else false)" false
+
+let%test "test_bigstep6" = test_bigstep "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
+
+let%test "test_bigstep7" = test_bigstep "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
+
+let%test "test_bigstep8" = test_bigstep "not not true" true
+
+let%test "test_bigstep9" = test_bigstep "not if true then false else true" true
+
+let%test "test_bigstep10" = test_bigstep "if true then (if true then false else true) else (if true then true else false)" false
+
+let%test "test_bigstep11" = test_bigstep "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
+
+let%test "test_bigstep12" = test_bigstep "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
+
+let%test "test_bigstep13" = test_bigstep "not true or true" true
+
+let%test "test_bigstep14" = test_bigstep "not true and false" false
+
+let%test "test_bigstep15" = test_bigstep "false and false or true" true
+
+let%test "test_bigstep16" = test_bigstep "true or false and false" true
+
+let%test "test_bigstep17" = test_bigstep "if true then true else false and false" true
+
+let%test "test_bigstep18" = test_bigstep "if true then false else false or true" false
 
 
 (**********************************************************************
@@ -76,19 +59,9 @@ let eval_smallstep e = match last (trace e) with
   | False -> Some false
   | _ -> None
 
-let%test _ =
-  print_newline();  
-  print_endline ("*** Testing small-step semantics...");
-  List.fold_left
-    (fun b (s,v) ->
-       print_string (s ^ " -> ");       
-       let ar = s |> parse |> eval_smallstep in
-       print_string (string_of_val ar);
-       let b' = (ar = Some v) in
-       if b' then print_string(" [OK]")
-       else print_string (" [NO: expected " ^ string_of_bool v ^ "]");
-       print_newline();
-       b && b')
-    true
-    tests
+let test_smallstep expr exp_result =
+  (expr |> parse |> eval_smallstep) = exp_result
+
+let%test "test_smallstep1" = test_smallstep "false" false
+
 

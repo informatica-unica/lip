e7f321e05c1fea529890c0823de9c07488125ee5
Federico <dalpio23@gmail.com>
2023-12-29 13:09:23 +0100

    Clean prettyprint.ml

--- imp/lip23/lib/prettyprint.ml
+++ imp/lip23/lib/prettyprint.ml
@@ -1,6 +1,5 @@
 open Ast
 open Memory
-open Intrinsic
 open Robot
 open Printf
 
@@ -24,10 +23,6 @@ let string_of_bop = function
 let string_of_uop = function
   | UMINUS -> "-"
 
-let string_of_postfix_op = function
-  | INCR -> "++"
-  | DECR -> "--"
-
 let rec string_of_expr = function
   | NIL -> "nil"
   | IDE x -> x
@@ -39,7 +34,6 @@ let rec string_of_expr = function
   | UNARY_EXPR (uop, e) -> spr "%s%s" (string_of_uop uop) (string_of_expr e)
   | BINARY_EXPR (e1, bop, e2) ->
       spr "%s %s %s" (string_of_expr e1) (string_of_bop bop) (string_of_expr e2)
-  | POSTFIX_EXPR (x, pop) -> spr "%s%s" x (string_of_postfix_op pop)
 
 and string_of_instr = function
   | EMPTY -> ";"
@@ -62,10 +56,6 @@ and string_of_instr = function
       spr "fun %s(%s) { %s }" x (String.concat "," pars) (string_of_instr s)
   | SEQ (s1, s2) -> spr "%s %s" (string_of_instr s1) (string_of_instr s2)
 
-let string_of_memory mem =
-  Hashtbl.fold (fun l v acc -> spr "%d/%d" l v :: acc) mem []
-  |> String.concat ", " |> spr "[%s]"
-
 let string_of_intrinsic = function
   | SCAN -> "scan"
   | CANNON -> "canon"
@@ -81,41 +71,13 @@ let string_of_intrinsic = function
   | TAN -> "tan"
   | ATAN -> "atan"
 
-let string_of_memval = function
-  | Loc l -> spr "%d" l
-  | Fun _ | Intrinsic _ -> "<fun>"
-
-let rec remove_duplicates s =
-  match Seq.uncons s with
-  | None -> Seq.empty
-  | Some ((x, v), s') ->
-      let s' = Seq.drop_while (fun (y, _) -> y = x) s' in
-      Seq.cons (x, v) (remove_duplicates s')
-
-let string_of_envrmt env =
-  let s = Hashtbl.to_seq env |> remove_duplicates in
-  Seq.fold_left
-    (fun acc (x, v) ->
-      match v with
-      | Intrinsic _ -> acc
-      | _ -> spr "%s/%s" x (string_of_memval v) :: acc)
-    [] s
-  |> String.concat ", " |> spr "{%s}"
-
 let string_of_trace es =
   List.map (fun e -> spr "%s" (string_of_expr e)) es |> String.concat "\n"
 
-let string_of_trace_st es =
-  List.map
-    (fun (env, mem, e) ->
-      spr "%s %s %s" (string_of_expr e) (string_of_envrmt env)
-        (string_of_memory mem))
-    es
-  |> String.concat "\n"
-
 let string_of_robot (r : Robot.t) =
-  spr "x: %3d, y: %3d, sp: %3d, dsp: %3d, acc: %3d, hd: %3d, dmg: %3d, sc: %3d, rel: %3d" r.x r.y
-    r.speed r.d_speed r.accel r.heading r.damage r.scan_degrees r.reload
+  spr
+    "x: %3d, y: %3d, sp: %3d, dsp: %3d, acc: %3d, hd: %3d, dmg: %3d, sc: %3d, rel: %3d"
+    r.x r.y r.speed r.d_speed r.accel r.heading r.damage r.scan_degrees r.reload
 
 let string_of_all_robots (rs : Robot.t array) =
   Array.to_seq rs

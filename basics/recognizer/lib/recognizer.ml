0d606eef28dba005e4df3fb066cbcf2c75cfc78e
Docker <docker@example.com>
2024-10-09 15:35:35 +0100

    dylan gay

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,8 +1,29 @@
-let lang1 _ = failwith ""
+let rec lang1 l = match l with 
+  []->false
+  |[0]->true
+  |[1]->true
+  |0::tl ->lang1 tl
+  |1::tl -> lang1 tl
+  |_::tl ->false
 
-let lang2 _ = failwith ""
+;;
 
-let lang3 _ = failwith ""
+let rec lang2 l2 = match l2 with 
+  []->true
+  |[0]->true
+  |[1]->true
+  |1::tl -> lang2 tl 
+  |0::tl -> List.for_all(fun x -> x <> 0) tl
+  |_::tl ->false
+
+;;
+
+let rec lang3 l3 = match l3 with
+  []->false
+  |0::tl -> (match List.rev tl with
+              | 0::tl -> List.for_all (fun x -> x = 0 || x = 1) tl
+              | _ -> false)
+  |_->false
 
 let lang4 _ = failwith ""
 

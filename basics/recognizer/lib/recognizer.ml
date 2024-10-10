bc2ae91a90c1827347f36fe54d76ac1ee98e7f13
Docker <docker@example.com>
2024-10-10 15:31:58 +0100

    lang

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -20,14 +20,30 @@ let rec lang2 l2 = match l2 with
 
 let rec lang3 l3 = match l3 with
   []->false
-  |0::tl -> (match List.rev tl with
-              | 0::tl -> List.for_all (fun x -> x = 0 || x = 1) tl
+  |0::tl -> (match List.rev l3 with
+              | 0::tl -> List.for_all (fun x -> x = 0 || x = 1) l3
               | _ -> false)
   |_->false
 
-let lang4 _ = failwith ""
+let rec lang4C l4 c = match l4 with
+  []-> if c = 2 then true else false
+  |0::tl -> lang4C tl c
+  |1::tl -> if c < 2 then lang4C tl (c+1) else false
+  |_::tl -> false
 
-let lang5 _ = failwith ""
+let lang4 l4 = 
+  lang4C l4 0
+
+  let rec check_lang5 l5 =  match l5 with
+  []->true
+  | 0::0::tl -> check_lang5 tl 
+  | 1::1::tl -> check_lang5 tl
+  | _ -> false
+
+let lang5 l5 =
+    if l5 = [] then false  
+    else if List.length l5 mod 2 = 1 then false else check_lang5 l5 
+  
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   

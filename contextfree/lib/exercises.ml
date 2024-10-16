e975c5db181d2620928237e986d51a392510b171
Docker <docker@example.com>
2024-10-16 15:23:35 +0100

    3 lezione

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,15 +16,54 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar =
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1"; (* Recursive step to generate equal numbers of 0s and 1s *)
+        S --> "";     (* Base case: when n = 0, produces the empty string *)
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
+let palindromes : grammar = {
+  symbols = [ S ];
+  terminals = [ '0'; '1' ];
+  productions =
+    [
+      S --> "0S0"; 
+      S --> "1S1";
+      S --> "0";
+      S --> "1";
+      S --> "";     (* Base case: when n = 0, produces the empty string *)
+    ];
+  start = S;
+}
 
 
 (* #### Exercise 3, medium (balanced_parentheses)*)
-let balanced_parentheses : grammar = todo
+let balanced_parentheses : grammar = {
+  symbols = [ S ];
+  terminals = [ '('; ')';'[';']' ; '{';'}' ];
+  productions =
+    [
+      S --> "(S)"; (*0*)
+      S --> "[S]"; (*1*)
+      S --> "{S}"; (*2*)
+      S --> "()S";  (*3*)
+      S --> "[]S"; (*4*)
+      S --> "{}S"; (*5*)
+      S --> "S()";  (*6*)
+      S --> "S[]"; (*7*)
+      S --> "S{}"; (*8*)
+      S --> "";     (* Base case: when n = 0, produces the empty string *)
+    ];
+  start = S;
+}
 
 
 (* #### Exercise 4, hard (same_amount)
@@ -33,4 +72,18 @@ let balanced_parentheses : grammar = todo
    Hint 2: think of the language of words where the number of 0s is one greater
    than the number of 1s and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = {
+  symbols = [ S ];
+  terminals = [ '0';'1' ];
+  productions =
+    [
+      S --> "0S0"; (*0*)
+      S --> "1S1"; (*1*)
+      S --> "0S1"; (*2*)
+      S --> "1S0";  (*3*)
+      S --> "";     (* Base case: when n = 0, produces the empty string *)
+    ];
+  start = S;
+}
+
+

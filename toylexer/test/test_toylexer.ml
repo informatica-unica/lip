a610a4687d8de840338396439483a6169d6e0c2e
Federico <dalpio23@gmail.com>
2024-10-23 18:17:02 +0200

    fix a test in toylexer/test

--- toylexer/test/test_toylexer.ml
+++ toylexer/test/test_toylexer.ml
@@ -2,6 +2,6 @@ open Toylexer.Token
 open Toylexer.Main
 
 let%test "test_frequencies_1" =
-  lexer "x=1; y=x+1" |> frequency 3 = [(ID "x", 3); (ASSIGN, 2); (ID "y", 1)]
+  lexer "x=y; x=x+1" |> frequency 3 = [(ID "x", 3); (ASSIGN, 2); (ID "y", 1)]
 
 (* YOUR TESTS HERE *)

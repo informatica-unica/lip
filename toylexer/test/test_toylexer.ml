23eda10486e55095ff2fcd2de8bdb5b3fed40c04
ariannamurtas <ariannamurtas01@gmail.com>
2024-10-28 11:57:27 +0100

    altri test toylexer

--- toylexer/test/test_toylexer.ml
+++ toylexer/test/test_toylexer.ml
@@ -4,10 +4,22 @@ open Toylexer.Main
 let%test "test_frequencies_1" =
   lexer "x=1; y=x+1" |> frequency 3 = [(ID "x", 3); (ASSIGN, 2); (ID "y", 1)] ;;
 
+
+let%test "test_frequencies_2" =
+  lexer "x=y; x=x+1" |> frequency 3 = [(CTOK "x", 3); (ASSIGN, 2); (PLUS, 1)] ;;
+
+
+let%test "test_frequencies_3" =
+  lexer "x=y; x=x+1" |> frequency 1 = [(CTOK "x", 3)] ;;
+
+
+let%test "test_frequencies_4" =
+  lexer "e=o; e=e+1" |> frequency 2 = [(BTOK "e", 3); (ASSIGN, 2)] ;;
+
 (* YOUR TESTS HERE *)
 
 let%test "test_freq_2" =
-  lexer "x=y; x=x+1" |> frequency 2 = [(ID "x", 3); (ASSIGN, 2)];;
+  lexer "x=y; x=x+1" |> frequency 2 = [(CTOK "x", 3); (ASSIGN, 2)];;
 
 let%test "test_token" =
   lexer "Abc123" = [ATOK "Abc123"; EOF];;

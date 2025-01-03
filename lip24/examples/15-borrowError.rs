949c74b8df534e74f1a2b40efaf7a5ffe0cb5088
Federico <dalpio23@gmail.com>
2025-01-03 21:06:54 +0100

    reorganize examples

--- lip24/examples/15-borrowError.rs
+++ lip24/examples/15-borrowError.rs
@@ -1,6 +1,10 @@
 fn main() {
     let mut x = String::from("Ciao");
     let y = &x;
-    x.push_str(", mondo"); // errore: y non mutabile
+    x.push_str(", mondo"); // errore: cannot borrow x as mutable because it is also borrowed as immutable
     println!("{y}");
 }
+
+// Note: push_str borrows x as mutable
+// signature of push_str:
+// pub fn push_str(&mut self, string: &str)
